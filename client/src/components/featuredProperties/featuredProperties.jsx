07d73ca7225e963caf6a3e5a4356f7c4fcd313de
chamaraAdikari <168882735+chamaraAdikari@users.noreply.github.com>
2025-07-03 01:07:58 +0530

    Update featuredProperties.jsx

--- client/src/components/featuredProperties/featuredProperties.jsx
+++ client/src/components/featuredProperties/featuredProperties.jsx
@@ -5,15 +5,11 @@ import { useCurrency } from "../../context/CurrencyContext";
 import LoadingAnimation from "../loadingAnimation/LoadingAnimation";
 
 const FeaturedProperties = () => {
-    const { data, loading, error } = useFetch("/hotels?featured=true");
-    const { formatPrice } = useCurrency();
-
-    if (loading) return <LoadingAnimation />;
-    if (error) return <div>Error loading featured properties: {error.message}</div>;
+    
 
     return (
         <div className="fp">
-            
+            hi
         </div>
     );
 };
